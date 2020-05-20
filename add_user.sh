9da88ce856f91996a1ae3221007d3701ac0ab207
Matheus Xavier <mxavier@bpmx.io>
2020-05-20 16:30:45 -0300

    improvements to user creation script

--- add_user.sh
+++ add_user.sh
@@ -1,15 +1,37 @@
 #!/bin/sh
 
-if [ -z ${user+x} ]; then exit; else echo "user is set to ${user}"; fi
-if [ -z ${pubkey+x} ]; then exit; else echo "pubkey is set to ${user}"; fi
-if [ -f /etc/dnf/dnf.conf ]; then sudogroup="wheel"; else sudogroup="sudo"; fi
+if [ -z "$NEW_USER" ]; then
+	return
+else
+	user="$NEW_USER"
+	printf 'user is set to %s' "$user"
+fi
+
+if [ -z "$SUDOGROUP" ]; then
+	sudogroup="$SUDOGROUP"
+else 
+	if [ -f "/etc/dnf/dnf.conf" ]; then
+		printf "found dnf configuration file assuming RHEL default of \'wheel\'"
+		sudogroup="wheel"
+	fi;
+	if [ -d "/etc/apt/apt.conf/" ]; then
+		printf "found apt files assuming debian derived distro and using default of \'sudo\' group"
+		sudogroup="sudo"
+	fi
+fi
 
 
-useradd -m -s /bin/bash -G $sudogroup $user
-mkdir -p "/home/$user/.ssh"
-chmod 700 /home/$user/.ssh
-echo "$pubkey" > /home/$user/.ssh/authorized_keys
-chmod 644 /home/$user/.ssh/authorized_keys
-chown -R "${user}:${user}" /home/$user/.ssh
-passwd -d $user
-passwd -e $user 
+useradd -m -s /bin/bash -G "$sudogroup" "$user"
+if [ -z "$PUBKEY" ]; then 
+	printf 'no pubkey given, ssh access will not be setup for the user %s' "$user"
+else 
+	pubkey="$PUBKEY"
+	echo "pubkey is set for ${user} and ssh access will be setup"
+	mkdir -p "/home/$user/.ssh"
+	chmod 700 "/home/$user/.ssh"
+	echo "$pubkey" > "/home/$user/.ssh/authorized_keys"
+	chmod 644 "/home/$user/.ssh/authorized_keys"
+	chown -R "${user}:${user}" "/home/$user/.ssh"
+fi
+passwd -d "$user"
+passwd -e "$user"
